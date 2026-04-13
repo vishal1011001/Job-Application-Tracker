ef8cfb40b187814e683faa33bedc6b562ea00978
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-04-13 22:52:57 +0530

    fixed Import Bugs.

--- job-application-tracker-backend/Routes/userRouter.js
+++ job-application-tracker-backend/Routes/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { registerUser, loginUser, getUser } from "../controllers/userController";
-import { protect } from "../middleware/authMiddleware";
+import { registerUser, loginUser, getUser } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
