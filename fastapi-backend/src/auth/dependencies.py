61e751fbd55bfe5b594ec6547c882ed15af00614
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-06-29 20:27:56 +0530

    Setup dependencies for Token Verification (AccessTokenBearer). Initialized alembic Migrations

---
+++ fastapi-backend/src/auth/dependencies.py
@@ -0,0 +1,21 @@
+from fastapi.security import HTTPBearer
+from fastapi.security.http import HTTPAuthorizationCredentials
+from fastapi import Request, status
+from .utils import decode_token
+from fastapi.exceptions import HTTPException
+
+class AccessTokenBearer(HTTPBearer):
+    def __init__(self, auto_error=True):
+        super().__init__(auto_error=auto_error)
+        
+    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+        creds = await super().__call__(request)
+        
+        token = creds.credentials
+        token_data = decode_token(token)
+          
+        if token_data:
+            return creds       
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access not authorized.")
+      
