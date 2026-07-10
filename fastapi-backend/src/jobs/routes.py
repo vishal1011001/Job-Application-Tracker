c63d8fe7da525badc8b45f072b617004f7934894
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-07-10 13:01:36 +0530

    Injected role checker depedency in job routes. Migrated database changes.

--- fastapi-backend/src/jobs/routes.py
+++ fastapi-backend/src/jobs/routes.py
@@ -4,13 +4,14 @@ from src.db.main import get_session
 from src.jobs.service import JobService
 from .schemas import CreateJobModel, JobModel, UpdateJobModel
 from fastapi.exceptions import HTTPException
-from src.auth.dependencies import AccessTokenBearer
+from src.auth.dependencies import AccessTokenBearer, RoleChecker
 
 job_router = APIRouter()
 job_service = JobService()
 access_token_bearer = AccessTokenBearer()
+role_checker = Depends(RoleChecker(['user', 'admin']))
 
-@job_router.get('/')
+@job_router.get('/', dependencies=[role_checker])
 async def get_all_jobs(
     session: AsyncSession = Depends(get_session),
     token_details: dict = Depends(access_token_bearer)                       
@@ -18,7 +19,7 @@ async def get_all_jobs(
     jobs = await job_service.get_all_jobs(session)
     return jobs if jobs is not None else {}
 
-@job_router.get('/{job_uid}')
+@job_router.get('/{job_uid}', dependencies=[role_checker])
 async def get_job(
     job_uid: str, 
     session: AsyncSession = Depends(get_session),
@@ -31,7 +32,7 @@ async def get_job(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Job searched for, not found.")
 
-@job_router.post('/', status_code=status.HTTP_201_CREATED, response_model=JobModel)
+@job_router.post('/', status_code=status.HTTP_201_CREATED, response_model=JobModel, dependencies=[role_checker])
 async def create_job(
     job_data: CreateJobModel,
     session: AsyncSession = Depends(get_session),
@@ -40,7 +41,7 @@ async def create_job(
     new_job = await job_service.create_job(job_data, session)
     return new_job
 
-@job_router.patch('/{job_uid}')
+@job_router.patch('/{job_uid}', dependencies=[role_checker])
 async def update_job(
     job_uid:str, 
     job_update_data: UpdateJobModel,
@@ -53,7 +54,7 @@ async def update_job(
     else: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Job to be updated not found.")
 
-@job_router.delete('/{job_uid}')
+@job_router.delete('/{job_uid}', dependencies=[role_checker])
 async def delete_job(
     job_uid: str,
     session: AsyncSession = Depends(get_session),
