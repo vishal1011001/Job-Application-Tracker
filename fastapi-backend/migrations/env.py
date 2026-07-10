8fec067fd44e8fe57a0002a1dda93ae5cc075d00
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-07-10 12:59:59 +0530

    Setup alembic settings for migrations

--- fastapi-backend/migrations/env.py
+++ fastapi-backend/migrations/env.py
@@ -6,10 +6,16 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from src.config import Config
+from sqlmodel import SQLModel
+
+database_url = Config.DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option('sqlalchemy.url', database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
